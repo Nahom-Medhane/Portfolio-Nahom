f24b3d39eaf1700544762c7de51300720b2ecbe0
Nahom medhane <nahommedhanege@gmail.com>
2020-08-12 17:04:49 +0200

    intiall commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 console.log('Its working')
 
 let theme = localStorage.getItem('theme')
@@ -38,45 +37,4 @@ function setTheme(mode){
 	}
 
 	localStorage.setItem('theme', mode)
-=======
-console.log('Its working')
-
-let theme = localStorage.getItem('theme')
-
-if(theme == null){
-	setTheme('light')
-}else{
-	setTheme(theme)
-}
-
-let themeDots = document.getElementsByClassName('theme-dot')
-
-
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
-}
-
-function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
-
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
-	}
-
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
-
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
-
-	localStorage.setItem('theme', mode)
->>>>>>> 4b463e3d3ca8040f83c1142115499faeef776a4f
 }
